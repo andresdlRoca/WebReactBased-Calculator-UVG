067435d1b774dbd5b689ab83a60724ced0a69b88
andresdlRoca <andresdelaroca11@gmail.com>
2022-04-30 12:43:09 -0600

    Calculator body created and animations added

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useRef, use } from "react";
+import "./styles/Calculator.css";
+
+export default function Calculator() {
+    return (
+      <div className="CalculatorBody">
+        <div className="overlayAesthethic"></div>
+      </div>
+  
+    );
+  }
--- src/components/Pagewrap.jsx
+++ src/components/Pagewrap.jsx
@@ -1,9 +1,12 @@
-
+import React from "react";
+import backgroundIMG from "../assets/background.jpg";
+import Calculator from "./Calculator";
+import './styles/Pagewrap.css';
 
 export default function Pagewrap() {
     return (
-      <div className="page-wrap">
-          
+      <div className="page-wrap" style={{ backgroundImage: `url(${backgroundIMG})` }}>
+        <Calculator />
       </div>
   
     );
